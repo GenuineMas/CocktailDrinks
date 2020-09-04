7cfa9d9b0dfee18702db6b3647b9ca89fd811f21
Genuine <MasalitinJ@gmail.com>
2020-09-04 15:06:27 +0300

    Initial Commit

---
+++ CocktailDrinks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailDrinks
+//
+//  Created by Genuine on 04.09.2020.
+//  Copyright © 2020 Genuine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
